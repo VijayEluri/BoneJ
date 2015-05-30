31a42bd1de2bdf6d39763161071dedc9902641fc
Michael Doube <michael@doube.net>
2015-05-30 22:06:05 +0100

    EF: use hard-coded 3x3 matrix inverse rather than JAMA's Matrix.inv()
    turn() is spending a lot of time calculating inverses, mainly due to a
    hideously unoptimised identity() method in jama.Matrix, and Matrix
    object instantiation.

--- src/org/bonej/EllipsoidFactor.java
+++ src/org/bonej/EllipsoidFactor.java
@@ -45,7 +45,6 @@ import ij3d.Image3DUniverse;
 import org.doube.geometry.Trig;
 import org.doube.geometry.Vectors;
 import org.doube.geometry.Ellipsoid;
-import org.doube.jama.Matrix;
 import org.doube.skeleton.Skeletonize3D;
 import org.doube.util.ArrayHelper;
 import org.doube.util.ImageCheck;
@@ -1333,8 +1332,8 @@ public class EllipsoidFactor implements PlugIn, Comparator<Ellipsoid> {
 		contactPoints = findEllipseContactPoints(ellipsoid, pixels, pW, pH, pD,
 				w, h, d);
 
-//		if (contactPoints == null)
-//			return null;
+		// if (contactPoints == null)
+		// return null;
 
 		if (contactPoints.size() > 0) {
 			double[] torque = calculateTorque(ellipsoid, contactPoints);
@@ -1419,7 +1418,7 @@ public class EllipsoidFactor implements PlugIn, Comparator<Ellipsoid> {
 		final double u = 2 / (c * c);
 
 		final double[][] rot = ellipsoid.getRotation();
-		final double[][] inv = (new Matrix(rot)).inverse().getArrayCopy();
+		final double[][] inv = inv(rot);
 
 		double t0 = 0;
 		double t1 = 0;
@@ -1535,7 +1534,7 @@ public class EllipsoidFactor implements PlugIn, Comparator<Ellipsoid> {
 		}
 
 		double f = Math.max((1 - 2 * pixelDiagonal / minContactLength), 0.25);
-		
+
 		ellipsoid.scale(f);
 
 		return ellipsoid;
@@ -2120,6 +2119,49 @@ public class EllipsoidFactor implements PlugIn, Comparator<Ellipsoid> {
 		int[][] skeletonPoints = list.toArray(new int[list.size()][]);
 
 		return skeletonPoints;
+	}
+
+	/**
+	 * <p>
+	 * Calculate the matrix inverse A<sup>-1</sup> of a 3x3 matrix A
+	 * </p>
+	 * 
+	 * @param a
+	 *            3x3 matrix
+	 * @return inverse of the 3x3 matrix
+	 * @see http://mathworld.wolfram.com/MatrixInverse.html
+	 * @see http://mathworld.wolfram.com/Determinant.html
+	 */
+	private double[][] inv(double[][] a) {
+		final double a00 = a[0][0];
+		final double a01 = a[0][1];
+		final double a02 = a[0][2];
+		final double a10 = a[1][0];
+		final double a11 = a[1][1];
+		final double a12 = a[1][2];
+		final double a20 = a[2][0];
+		final double a21 = a[2][1];
+		final double a22 = a[2][2];
+
+		// calculate the inverse of the determinant of a
+		final double invDetA = 1 / (a00 * a11 * a22 - a00 * a12 * a21 - a01
+				* a10 * a22 + a01 * a12 * a20 + a02 * a10 * a21 - a02 * a11
+				* a20);
+
+		// calculate square 2x2 determinants
+		double[][] b = new double[3][3];
+		
+		b[0][0] = (a11 * a22 - a12 * a21) * invDetA;
+		b[0][1] = (a02 * a21 - a01 * a22) * invDetA;
+		b[0][2] = (a01 * a12 - a02 * a11) * invDetA;
+		b[1][0] = (a12 * a20 - a10 * a22) * invDetA;
+		b[1][1] = (a00 * a22 - a02 * a20) * invDetA;
+		b[1][2] = (a02 * a10 - a00 * a12) * invDetA;
+		b[2][0] = (a10 * a21 - a11 * a20) * invDetA;
+		b[2][1] = (a01 * a20 - a00 * a21) * invDetA;
+		b[2][2] = (a00 * a11 - a01 * a10) * invDetA;
+
+		return b;
 	}
 
 	/**
